b595322670494b56b376e039bc09af429602436b
Suarmin Rais <suarmin.rais@gmail.com>
2021-03-08 23:57:29 +0800

    set model db order

--- database/migrations/2021_03_08_155412_create_orders_table.php
+++ database/migrations/2021_03_08_155412_create_orders_table.php
@@ -15,6 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('program_id');
+            $table->foreignId('user_id');
+            $table->integer('total');
             $table->timestamps();
         });
     }
